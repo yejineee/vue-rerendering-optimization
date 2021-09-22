9ffcc2240f80bfed28306cb76491d37721908030
lillie.yang <lillie.yang@kakaocorp.com>
2021-09-22 22:49:14 +0900

    Feat: 스토어 구조 추가

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import * as example1 from './example1'
 import * as example2 from './example2'
 import * as example3 from './example3'
 import * as example4 from './example4'
+import * as yejin from './yejin'
 
 Vue.use(Vuex)
 
@@ -12,6 +13,7 @@ export default new Vuex.Store({
     example1: { ...example1, namespaced: true },
     example2: { ...example2, namespaced: true },
     example3: { ...example3, namespaced: true },
-    example4: { ...example4, namespaced: true }
+    example4: { ...example4, namespaced: true },
+    yejin: { ...yejin, namespaced: true }
   }
 })
